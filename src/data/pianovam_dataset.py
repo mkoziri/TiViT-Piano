e1ec0b1cba5a5571e2afacdeb6ee801aefa380c1
Tasos Chatzigiannis <93002145+TasosC98@users.noreply.github.com>
2025-11-23 22:17:52 +0200

    Αdd placeholder PianoVAMDataset

---
+++ src/data/pianovam_dataset.py
@@ -0,0 +1,6 @@
+class PianoVAMDataset:
+    """Placeholder dataset for PianoVAM.
+    TODO: implement reading from PianoVAM_v1.0 metadata and assets.
+    """
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("PianoVAMDataset is not fully implemented yet.")
